5fdc20d20fdacc256018ba1c6e266f726522cfcd
Abdullah Diaa <abdullahdiee@gmail.com>
2024-06-08 01:09:59 +0300

    Add files via upload

---
+++ shop/js/main.js
@@ -0,0 +1,23 @@
+// let x=document.querySelector(" ");
+// let x=document.querySelector(" ");
+// let x=document.querySelector(" ");
+// let x=document.querySelector(" ");
+// let x=document.querySelector(" ");
+
+// let x=document.querySelectorAll(" ");
+// let x=document.querySelectorAll(" ");
+// let x=document.querySelectorAll(" ");
+// let x=document.querySelectorAll(" ");
+
+// test
+// console.log(object);
+
+// x.onlick=_>{
+
+// }
+
+// array.forEach(e => {
+//     addEventListener("click",_=>{
+
+//     })
+// });
